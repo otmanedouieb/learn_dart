22818a173200a6a97ad1ad2aeb7fced84f53dbf8
otmanedouieb <douieb197@gmail.com>
2024-06-12 00:33:13 +0100

    Create strings.dart
    string data type

---
+++ strings.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("Strings:");
+  print("========================");
+  var name = "otmane douieb";
+  var age = 28;
+  print("Hello, my name is $name and my age is $age.");
+
+  print("My name has ${name.length - 1} characters.");
+}
