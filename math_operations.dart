80757d019eb2ec9f5d3cdeefb872dee2f3b9e6da
otmanedouieb <douieb197@gmail.com>
2024-06-12 00:32:43 +0100

    Create math_operations.dart
    simple math operations

---
+++ math_operations.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+void main() {
+  /**
+   * In this section we learn how to use math operations
+   */
+  print("Simple math operations:");
+  // Addition
+  print(1 + 1);
+  // Subtraction
+  print(5 - 3);
+  // Multiplication
+  print(5 * 3);
+  // Division
+  print(5 / 3);
+
+  print("Use math lib:");
+  // power
+  print(pow(2, 3));
+}
