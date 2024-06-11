c18e7450822a51de609a7194f0d4c7781cb004bc
otmanedouieb <douieb197@gmail.com>
2024-06-12 00:32:28 +0100

    Create data_types.dart
    data types in dart

---
+++ data_types.dart
@@ -0,0 +1,27 @@
+void main() {
+  print("Built-in data types in dart:");
+  print("============================");
+  print("Numbers: int, double");
+  print(10);
+  print(1.5);
+  print("Strings: string");
+  print("This is  string");
+  print("Boolean: bool");
+  print(true);
+  print(false);
+  print("Lists: list");
+  print("Sets: set");
+  print("Maps: map");
+  print("Null: null");
+  print("Runes: runes");
+  print("Symbols: symbol");
+
+  print("Any data type have properties and methods:");
+  print("============================");
+  print("Example with string");
+  var name = "otmane douieb";
+  print(name);
+  print(name.length);
+  print(name.isEmpty);
+  print(name.toUpperCase());
+}
